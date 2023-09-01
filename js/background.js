542987c41eb2ad2b223e4844b7819af38e87934f
l1n6yun <l1n6yun@gmail.com>
2023-09-01 19:19:16 +0800

    添加语音

--- js/background.js
+++ js/background.js
@@ -2,8 +2,11 @@
  * 创建菜单
  */
 chrome.contextMenus.removeAll();
-chrome.contextMenus.create({"title": "朗读", "contexts": ["selection"], "onclick": speak});
-
+chrome.contextMenus.create({
+    "title": "朗读",
+    "contexts": ["selection"],
+    "onclick": speak
+});
 /**
  * speak 方法
  * @param selection
@@ -12,7 +15,6 @@ function speak(selection) {
     var utterance = selection.selectionText.toString()
     tts_speak(utterance)
 }
-
 /**
  * tts speak
  * @param utterance
@@ -25,22 +27,21 @@ function tts_speak(utterance) {
             'pitch': 1,
             'volume': 1,
             'gender': 'male',
+            'voiceName': 'Microsoft Huihui - Chinese (Simplified, PRC)',
         };
         localStorage.setItem("options", JSON.stringify(options));
     }
-    chrome.tts.speak(utterance,
-        {
-            rate: parseInt(options.rate),
-            pitch: parseInt(options.pitch),
-            volume: parseInt(options.volume),
-            gender: options.gender
-        }
-    );
+    chrome.tts.speak(utterance, {
+        rate: parseFloat(options.rate),
+        pitch: parseFloat(options.pitch),
+        volume: parseFloat(options.volume),
+        gender: options.gender,
+        voiceName: options.voiceName
+    });
 }
-
 /**
  * 监听请求事件
  */
-chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
     tts_speak(request)
 });
--- js/options.js
+++ js/options.js
@@ -1,51 +1,48 @@
-$(function () {
+$(function() {
     var options = JSON.parse(localStorage.getItem("options"));
-
     if (!options) {
-		options = {
-			'rate': 1,
-			'pitch': 1,
-			'volume': 1,
-			'gender': 'male',
-		};
-		localStorage.setItem("options", JSON.stringify(options));
+        options = {
+            'rate': 1,
+            'pitch': 1,
+            'volume': 1,
+            'gender': 'male',
+            'voiceName': 'Microsoft Huihui - Chinese (Simplified, PRC)',
+        };
+        localStorage.setItem("options", JSON.stringify(options));
     }
-
-
     $('#rate').slider({
         value: options.rate,
-        formatter: function (value) {
+        formatter: function(value) {
             return value;
         }
     });
     $('#pitch').slider({
         value: options.pitch,
-        formatter: function (value) {
+        formatter: function(value) {
             return value;
         }
     });
     $('#volume').slider({
         value: options.volume,
-        formatter: function (value) {
+        formatter: function(value) {
             return value;
         }
     });
     $('#gender').val(options.gender);
-
-    $("#submit").click(function () {
+    $('#voiceName').val(options.voiceName);
+    $("#submit").click(function() {
         var options = {};
         var t = $('form').serializeArray();
-        $.each(t, function () {
+        $.each(t, function() {
             options[this.name] = this.value;
         });
         localStorage.setItem("options", JSON.stringify(options));
         return false;
     });
-
-    $("#subtest").click(function () {
+    $("#subtest").click(function() {
         var options = {};
         var t = $('form').serializeArray();
-        $.each(t, function () {
+        $.each(t, function() {
             options[this.name] = this.value;
         });
         var utterance = $("#test").val();
@@ -53,7 +50,8 @@ $(function () {
             rate: parseFloat(options.rate),
             pitch: parseFloat(options.pitch),
             volume: parseFloat(options.volume),
-            gender: options.gender
+            gender: options.gender,
+            voiceName: options.voiceName
         });
         return false;
     });
